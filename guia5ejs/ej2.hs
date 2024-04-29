69bd12e27385c97f5240e2c63303c5815a96b47e
gpiero10 <163913172+gpiero10@users.noreply.github.com>
2024-04-28 21:20:03 -0300

    Update ej2.hs

--- guia5ejs/ej2.hs
+++ guia5ejs/ej2.hs
@@ -34,6 +34,35 @@ aux1 x (xs) | pertenece x (quitar x (xs)) == False = quitar x (xs)
             | pertenece x (quitar x (xs)) == True = aux1 x (quitar x (xs))
 
 eliminarRepetidos :: (Eq t) => [t] -> [t]   
+eliminarRepetidos (xs) | hayRepetidos xs == False = xs
+                       | hayRepetidos xs == True = aux2 xs
+
+aux2 :: (Eq t) => [t] -> [t] 
+aux2 (x:xs) | pertenece x (xs) == False = aux2 (xs)
+            | pertenece x (xs) == True && hayRepetidos (quitartodos x (x:xs)) == False = x:quitartodos x (x:xs)
+            | pertenece x (xs) == True && hayRepetidos (quitartodos x (x:xs)) == True = x:aux2(quitartodos x (x:xs)) 
+
+mismosElementos :: (Eq t) => [t] -> [t] -> Bool
+mismosElementos (xs) (ys) | hayRepetidos (xs) == False && hayRepetidos (ys) == False = auxii (xs) (ys)
+                          | hayRepetidos (xs) == True && hayRepetidos (ys) == False = auxii (eliminarRepetidos xs) (ys)
+                          | hayRepetidos (xs) == False && hayRepetidos (ys) == True = auxii (xs) (eliminarRepetidos ys)
+                          | hayRepetidos (xs) == True && hayRepetidos (ys) == True = auxii (eliminarRepetidos xs) (eliminarRepetidos ys) 
+
+auxii :: (Eq t) => [t] -> [t] -> Bool
+auxii [ ] (ys) = True
+auxii (x:xs) (ys) | pertenece x ys == False = False
+                  | pertenece x ys == True  = auxii (xs) (ys)
+
+capicua :: (Eq t) => [t] -> Bool
+capicua (xs) | (xs) == reverse(xs) = True
+             | otherwise = False   
+
+
+
+
+          
+                  
+                     
          
 
 
